0c66b9449c53f27cb0782be34f60a7e3e376d926
Bastian Mankel <bastian.mankel@gib.world>
2023-11-17 11:39:19 +0100

    ADD: ApiRequest

---
+++ src/controller/ApiRequests.ts
@@ -0,0 +1,25 @@
+export class ApiRequests{
+    private apiUrl = "https://api.openligadb.de"
+    public async getleagues(){
+        const result = await fetch(this.apiUrl + '/getavailableleagues', {
+            method: 'GET', 
+        }); 
+
+        if(result.ok){
+            console.log('')
+            return result.json(); 
+        }
+
+
+    }
+
+    public async getNextMatchesbyTeam(teamId: string, leagueId: string){
+        const result = await fetch(this.apiUrl + `/getnextmatchbyleagueteam/${leagueId}/${teamId}`, {
+            method: 'GET'
+        })
+
+        if(result.ok){
+            return result.json(); 
+        }
+    }
+}
