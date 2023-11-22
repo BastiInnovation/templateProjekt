a0ad3db07cc0d4375566b7e5282115905bfca559
Bastian Mankel <bastian.mankel@gib.world>
2023-11-22 10:48:47 +0100

    Add: Types for match

--- src/controller/Detail.controller.ts
+++ src/controller/Detail.controller.ts
@@ -3,6 +3,7 @@ import JSONModel from "sap/ui/model/json/JSONModel";
 import { ApiRequests } from "./ApiRequests";
 import BaseController from "./BaseController";
 import UI5Event from "sap/ui/base/Event"; 
+import { Match } from "../model/apiTypes";
 
 /**
  * @namespace ifm.bs.template.templateproject.controller
@@ -16,24 +17,15 @@ export default class Detail extends BaseController{
     private async onObjectMatched(event: UI5Event){
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         const path = decodeURIComponent(event.getParameter("arguments").path); 
-        const TeamId = ((this.getModel("bundesligaPlacing") as any).oData[path].teamInfoId) as number; 
-        console.log(TeamId)
+        const teamId = ((this.getModel("bundesligaPlacing") as any).oData[path].teamInfoId) as number; 
         const klasse = new ApiRequests();
-        const matchDays = await klasse.getAllMatches();
-        const matches: any[]  = [];
-        const groupes: any[]  = [];
-        for(const match of   matchDays){
-            if(TeamId == match.team1.teamId && match.matchIsFinished == true || TeamId == match.team2.teamId && match.matchIsFinished == true
-                || TeamId == match.team1.teamId && match.matchIsFinished == false || TeamId == match.team2.teamId && match.matchIsFinished == false){
-                matches.push(match);
-                groupes.push(match.group);
-            }  
+        const matchDays = await klasse.getAllMatches() as Match[];
+        const matches = matchDays.filter(match => match.team1.teamId === teamId || match.team2.teamId === teamId)
+        const matchModel = matches.map(match => ({
+            groupName: match.group.groupName,
+            steffan: match.team1.teamName,
+        }))
+        console.log(matches)
+        this.setModel(new JSONModel(matchModel), "spiele");
         }
-        this.setModel(new JSONModel(matches), "spiele");
-        console.log(groupes)
-        //this.setModel(new JSONModel(groupes), "gruppe");
-        
-        
-        //console.log(this.getOwnerComponent().getModel('spiele'));
-    }
 }
---
+++ src/model/apiTypes.ts
@@ -0,0 +1,64 @@
+export type Match ={
+    matchID: number;
+    matchDateTime: string;
+    timeZoneID: string;
+    leagueId: number;
+    leagueName: string;
+    leagueSeason: number;
+    leagueShortcut: string;
+    matchDateTimeUTC: string;
+    group: Group;
+    team1: Team; 
+    team2: Team; 
+    lastUpdateDateTime: string;
+    matchIsFinished: boolean;
+    matchResults: MatchResult;
+    goals: Goal[];
+    location: Location;
+    numberOfViewers: string; 
+  }
+
+  export type Team = {
+        teamId: number;
+        teamName: string;
+        shortName: string;
+        teamIconUrl: string;
+        teamGroupName: string;
+  }
+
+  export type Group = {
+    groupName: string;
+    groupOrderID: number; 
+    groupID: number; 
+  }
+
+  export type Goal = {
+    goalID: number;
+    scoreTeam1: number;
+    scoreTeam2: number; 
+    matchMinute: number; 
+    goalGetterID: number; 
+    goalGetterName: string;
+    isPenalty: boolean; 
+    isOwnGoal: boolean; 
+    isOvertime: boolean; 
+    comment: string; 
+  }
+
+  export type Location = {
+    locationID: number; 
+    locationCity: string; 
+    locationStadium: string; 
+  }
+
+  export type MatchResult = {
+    resultID: number;
+    resultName: string;
+    pointsTeam1: number;
+    pointsTeam2: number;
+    resultOrderID: number;
+    resultTypeID: number;
+    resultDescription: string
+  }
+
+  
